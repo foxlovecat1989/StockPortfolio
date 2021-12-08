0a6a666e264d99a36a0ee5e5bd4dbcc410e76b37
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-12-08 21:39:32 +0800

    implements get JWT token from backend

--- src/main/java/com/moresby/ed/stockportfolio/controller/ValidateUserController.java
+++ src/main/java/com/moresby/ed/stockportfolio/controller/ValidateUserController.java
@@ -1,14 +1,35 @@
 package com.moresby.ed.stockportfolio.controller;
 
+import com.moresby.ed.stockportfolio.service.JWTService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping(path = "api/v1/basicAuth")
+@RequiredArgsConstructor
 public class ValidateUserController {
 
-    @RequestMapping(path = "/validate")
-    public String userIsValid(){
-        return "{\"result\" : \"ok\"}";
+    private final JWTService jwtService;
+    private final int INDEX_OF_EXTRACT_ROLE_BEGIN = 5;
+
+    @RequestMapping(path = "validate")
+    public Map<String, String> userIsValid(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) auth.getPrincipal();
+        String name = currentUser.getUsername();
+        String role = currentUser.getAuthorities().toArray()[0].toString().substring(INDEX_OF_EXTRACT_ROLE_BEGIN);
+        String token = jwtService.generateToken(name, role);
+        Map<String, String> result = new HashMap<>();
+        result.put("result", token);
+
+        return result;
     }
 }
+
