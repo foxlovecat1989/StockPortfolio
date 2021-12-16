f17f2da267aa9f6cea920b5563ef52db13161034
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-12-16 20:03:13 +0800

    clean code

--- src/main/java/com/moresby/ed/stockportfolio/filter/JwtAccessDeniedHandler.java
+++ src/main/java/com/moresby/ed/stockportfolio/filter/JwtAccessDeniedHandler.java
@@ -6,7 +6,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -22,7 +21,7 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(
             HttpServletRequest request,
             HttpServletResponse response,
-            AccessDeniedException accessDeniedException) throws IOException, ServletException {
+            AccessDeniedException accessDeniedException) throws IOException{
         HttpResponse httpResponse =
                 new HttpResponse(
                         UNAUTHORIZED.value(),
--- src/main/java/com/moresby/ed/stockportfolio/filter/JwtAuthenticationAndAuthorizationFilter.java
+++ src/main/java/com/moresby/ed/stockportfolio/filter/JwtAuthenticationAndAuthorizationFilter.java
@@ -1,9 +1,8 @@
 package com.moresby.ed.stockportfolio.filter;
 
 import com.moresby.ed.stockportfolio.utility.JwtTokenProvider;
-import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,45 +12,41 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
 
 import static com.moresby.ed.stockportfolio.constant.SecurityConstant.OPTIONS_HTTP_METHOD;
 import static com.moresby.ed.stockportfolio.constant.SecurityConstant.TOKEN_PREFIX;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.OK;
-
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class JwtAuthenticationAndAuthorizationFilter extends OncePerRequestFilter {
 
-    private JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws IOException, ServletException {
-        if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) {
-            response.setStatus(OK.value());
-        } else {
+        if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD))
+            response.setStatus(HttpStatus.OK.value());
+        else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
-
             if (authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_PREFIX)) {
                 filterChain.doFilter(request, response);
                 return;
             }
-
             String token = authorizationHeader.substring(TOKEN_PREFIX.length());
             String username = jwtTokenProvider.getSubject(token);
+
             if (jwtTokenProvider.isTokenValid(username, token)
                     && SecurityContextHolder.getContext().getAuthentication() == null) {
-                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
-                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                var authorities = jwtTokenProvider.getAuthorities(token);
+                var authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else {
-                SecurityContextHolder.clearContext();
             }
+            else
+                SecurityContextHolder.clearContext();
         }
         filterChain.doFilter(request, response);
     }
