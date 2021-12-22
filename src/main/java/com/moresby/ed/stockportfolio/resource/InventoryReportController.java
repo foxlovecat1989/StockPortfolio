ac0afcdec6fb69342f60272c3b2c7f518d2ca552
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-12-23 00:28:44 +0800

    bug fix

--- src/main/java/com/moresby/ed/stockportfolio/resource/InventoryReportController.java
+++ src/main/java/com/moresby/ed/stockportfolio/resource/InventoryReportController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/report")
+@RequestMapping(path = "/api/v1/report")
 @RequiredArgsConstructor
 public class InventoryReportController {
 
