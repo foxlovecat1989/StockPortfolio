f15372708c71f6dd5cabad5e4f9a3d2cfea8265a
foxlovecat1989 <foxlovecat1989@gmail.com>
2022-01-04 01:28:00 +0800

    implements permission-based authorized in user controller

--- src/main/java/com/moresby/ed/stockportfolio/resource/UserController.java
+++ src/main/java/com/moresby/ed/stockportfolio/resource/UserController.java
@@ -16,12 +16,20 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
+import static com.moresby.ed.stockportfolio.constant.FileConstant.*;
 import static com.moresby.ed.stockportfolio.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static com.moresby.ed.stockportfolio.constant.UserImplConstant.EMAIL_SENT;
 import static com.moresby.ed.stockportfolio.constant.UserImplConstant.USER_DELETED_SUCCESSFULLY;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @RequestMapping(path = "/api/v1/user")
@@ -34,101 +42,94 @@ public class UserController extends UserExceptionHandling {
     private final AuthenticationManager authenticationManager;
 
     @GetMapping(path = "/findAll", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<User>> findAllUsers() throws InterruptedException {
-        Thread.sleep(3000); // TODO: remove when production
+    @PreAuthorize(value = "hasAnyAuthority('manage:read', 'admin:read')")
+    public ResponseEntity<List<User>> findAllUsers() {
         List<User> users = userService.findAllUsers();
 
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{userNumber}", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> findUserByUserNumber(@PathVariable String userNumber) throws InterruptedException, UserNotFoundException {
-        Thread.sleep(3000); // TODO: remove when production
+    @PreAuthorize(value = "hasAnyAuthority('user:read')")
+    public ResponseEntity<User> findUserByUserNumber(@PathVariable String userNumber)
+            throws UserNotFoundException {
         var user = userService.findExistingUserByUserNumber(userNumber);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping
+    @PreAuthorize(value = "hasAnyAuthority('manage:create', 'admin:create')")
     public ResponseEntity<User> createUser(@RequestBody User user)
-            throws InterruptedException, EmailExistException, UsernameExistException {
-        Thread.sleep(3000); // TODO: remove when production
+            throws EmailExistException, UsernameExistException {
         var newUser = userService.createUser(user);
 
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-//    @PostMapping("/updateProfileImage/{userNumber}")
-//    public ResponseEntity<User> updateProfileImage(
-//            @PathVariable("userNumber") String userNumber,
-//            @RequestParam(value = "profileImage") MultipartFile profileImage
-//    )
-//            throws
-//            UsernameExistException,
-//            EmailExistException,
-//            IOException,
-//            NotAnImageFileException, UserNotFoundException {
-//        User user = userService.updateProfileImage(userNumber, profileImage);
-//
-//        return new ResponseEntity<>(user, HttpStatus.OK);
-//    }
+    @PostMapping("/updateProfileImage/{userNumber}")
+    public ResponseEntity<User> updateProfileImage(
+            @PathVariable("userNumber") String userNumber,
+            @RequestParam(value = "profileImage") MultipartFile profileImage
+    )
+            throws
+            UsernameExistException,
+            EmailExistException,
+            IOException,
+            NotAnImageFileException, UserNotFoundException {
+        User user = userService.updateProfileImage(userNumber, profileImage);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
     @PatchMapping(path = "/update", consumes = APPLICATION_JSON_VALUE)
+    @PreAuthorize(value = "hasAnyAuthority('manage:update', 'admin:update')")
     public ResponseEntity<User> updateUser(@RequestBody User user)
-            throws InterruptedException, EmailExistException, UsernameExistException, UserNotFoundException {
-        Thread.sleep(3000); // TODO: remove when production
+            throws EmailExistException, UsernameExistException, UserNotFoundException {
         var updateUser = userService.updateUser(user);
 
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 
     @PatchMapping(path = "/modified", consumes = APPLICATION_JSON_VALUE)
+    @PreAuthorize(value = "hasAnyAuthority('user:update')")
     public ResponseEntity<User> updateUserNameOrEmail(@RequestBody User user)
-            throws InterruptedException, EmailExistException, UsernameExistException, UserNotFoundException {
-        Thread.sleep(3000); // TODO: remove when production
+            throws EmailExistException, UsernameExistException, UserNotFoundException {
         var updateUser = userService.updateUserNameOrEmail(user);
 
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{userNumber}")
-    @PreAuthorize("hasAnyAuthority('user:delete')")
+    @PreAuthorize("hasAnyAuthority('admin:delete')")
     public ResponseEntity<HttpResponse> deleteUser(@PathVariable("userNumber") String userNumber)
-            throws InterruptedException, UserNotFoundException {
-        Thread.sleep(3000); // TODO: remove when production
+            throws UserNotFoundException {
         userService.deleteUserByUserNumber(userNumber);
 
         return response(HttpStatus.NO_CONTENT, USER_DELETED_SUCCESSFULLY);
     }
 
-//    @GetMapping(path = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE)
-//    public byte[] getProfileImage(
-//            @PathVariable("username") String username,
-//            @PathVariable("fileName") String fileName) throws IOException {
-//
-//        return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
-//    }
-//
-//    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
-//    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
-//        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
-//        log.info(url.toString());
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        try (InputStream inputStream = url.openStream()) {
-//            int bytesRead;
-//            byte[] chunk = new byte[1024];
-//            while((bytesRead = inputStream.read(chunk)) > 0) {
-//                byteArrayOutputStream.write(chunk, 0, bytesRead);
-//            }
-//        }
-//
-//        return byteArrayOutputStream.toByteArray();
-//    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        log.info(url.toString());
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+
+        return byteArrayOutputStream.toByteArray();
+    }
 
     @GetMapping(path = "/reset/password/{email}")
+    @PreAuthorize("hasAnyAuthority('user:read')")
     public ResponseEntity<HttpResponse> resetPassword(@PathVariable("email") String email)
-            throws InterruptedException, UserNotFoundException {
-        Thread.sleep(3000); // TODO: remove when production
+            throws UserNotFoundException {
         System.out.println(email);
 
         userService.resetPassword(email);
