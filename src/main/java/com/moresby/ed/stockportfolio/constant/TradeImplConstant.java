4f93322dd414fab57f60128685d19a8145270796
foxlovecat1989 <foxlovecat1989@gmail.com>
2022-01-06 01:16:49 +0800

    remove useless code

--- src/main/java/com/moresby/ed/stockportfolio/constant/TradeImplConstant.java
+++ src/main/java/com/moresby/ed/stockportfolio/constant/TradeImplConstant.java
@@ -4,8 +4,6 @@ public class TradeImplConstant {
     public static final String INSUFFICIENT_BALANCE = "InSufficient Amount in Your Account, Your Remaining Balance is: %s";
     public static final String NO_ACCOUNT_FOUND_BY_USER_NUMBER = "No Bank Account Found by UserNumber: %s";
     public static final String INPUT_AMOUNT_CANNOT_BE_NEGATIVE = "Input Cannot be Negative Number %s";
-    public static final String NO_INVENTORY_FOUND_BY_USER_NUMBER_AND_STOCK_ID =
-            "No Inventory Found by UserNumber: %s And StockId: %s";
     public static final String INSUFFICIENT_AMOUNT_IN_INVENTORY =
             "InSufficient Amount In Your Inventory, Your remaining amount is: %s";
     public static final String NO_TRADE_FOUND_BY_TRADE_ID = "No Trade Found by TradeId: %s";
